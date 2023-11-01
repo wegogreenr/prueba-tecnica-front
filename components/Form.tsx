58675cfc908f0bcf32733eab1037da53a7d1651d
Matias Duran <matiduraan@gmail.com>
2023-11-01 11:14:38 -0300

    Initialize form

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,5 +1,24 @@
+import * as yup from "yup";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object().shape({
+  //
+});
+
 const Form = () => {
-  return <></>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = () => {};
+
+  return <form onSubmit={handleSubmit(onSubmit)}></form>;
 };
 
 export default Form;
